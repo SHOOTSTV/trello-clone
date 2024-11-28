54aede199117be8fc653446ce9d3c8c18d17244a
Axel <72924802+SHOOTSTV@users.noreply.github.com>
2024-11-28 17:41:55 +0100

    Board Popover Form

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -0,0 +1,31 @@
+import { FormPopover } from "@/components/form/form-popover";
+import { Hint } from "@/components/hint";
+import { HelpCircle, User2 } from "lucide-react";
+
+export const BoardList = () => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center text-lg font-semibold text-neutral-700">
+        <User2 className="mr-2 size-6" />
+        Your boards
+      </div>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+        <FormPopover side="right" sideOffset={10}>
+          <div
+            role="button"
+            className="relative flex aspect-video size-full flex-col items-center justify-center gap-y-1 rounded-sm bg-muted transition hover:opacity-75"
+          >
+            <p className="text-sm">Create new board</p>
+            <span className="text-xs">5 remaining</span>
+            <Hint
+              sideOffset={40}
+              description={`Free workspaces can have upto 5 open boards. For unlimited boards, please upgrade this workspace.`}
+            >
+              <HelpCircle className="absolute bottom-2 right-2 size-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
+      </div>
+    </div>
+  );
+};
---
+++ components/form/form-popover.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverClose,
+} from "@/components/ui/popover";
+import { createBoard } from "@/actions/create-board";
+import { useAction } from "@/hooks/use-actions";
+import { toast } from "sonner";
+
+import { FormInput } from "@/components/form/form-input";
+import { FormSubmit } from "@/components/form/form-submit";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+
+export const FormPopover = ({
+  children,
+  side = "bottom",
+  align,
+  sideOffset,
+}: FormPopoverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log({ data });
+      toast.success("Board created.");
+    },
+    onError: (error) => {
+      console.log({ error });
+      toast.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="pb-4 text-center text-sm font-medium text-neutral-600">
+          Create board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            variant="ghost"
+            className="absolute right-2 top-2 size-auto p-2 text-neutral-600"
+          >
+            <X className="size-4" />
+          </Button>
+        </PopoverClose>
+
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput
+              id="title"
+              label="Board title"
+              type="text"
+              errors={fieldErrors}
+            />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
