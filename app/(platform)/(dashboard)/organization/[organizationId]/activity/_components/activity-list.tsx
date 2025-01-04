a6cfc8f9c5c30f66b15a3c578a40d382c18af05f
Axel <72924802+SHOOTSTV@users.noreply.github.com>
2025-01-04 20:32:49 +0100

    fix: update skeleton widths and improve formatting in activity list and sheet components

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
@@ -29,10 +29,10 @@ ActivityList.Skeleton = function ActivityListSkeleton() {
   return (
     <ol className="mt-4 space-y-4">
       <Skeleton className="h-14 w-4/5" />
-      <Skeleton className="h-14 w-[50%]" />
+      <Skeleton className="h-14 w-1/2" />
       <Skeleton className="h-14 w-[70%]" />
       <Skeleton className="h-14 w-4/5" />
-      <Skeleton className="h-14 w-[75%]" />
+      <Skeleton className="h-14 w-3/4" />
     </ol>
   );
 };
